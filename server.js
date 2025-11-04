debe2efc2124788ac88c08c6539d9ef99d0760ca
SofyaB-code <68728293+SofyaB-code@users.noreply.github.com>
2025-11-04 20:44:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 // 🔑 ВСТАВЬ СЮДА СВОЙ WB API КЛЮЧ
-const API_TOKEN = "ВСТАВЬ_СВОЙ_API_КЛЮЧ_СЮДА";
+const API_TOKEN = "eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjUwOTA0djEiLCJ0eXAiOiJKV1QifQ.eyJhY2MiOjEsImVudCI6MSwiZXhwIjoxNzc4MDQzMjg3LCJpZCI6IjAxOWE0ZmNhLWUyYzEtN2MzYi1iMGJlLWNmNDFkOTgzMzA2YiIsImlpZCI6MjAxNTQ2MTksIm9pZCI6MjgyNzg0LCJzIjoxNjEyNiwic2lkIjoiNzZlZmM0NjktYTgxNS00NGEyLWIzMjEtNzMzNzI5ZjY0NjBlIiwidCI6ZmFsc2UsInVpZCI6MjAxNTQ2MTl9.8OIgbWiq044kkUgCWmvIaY9eg9j2qHxXSbsXwRWeQcUCnh4RcMSxVEkK7E08WI8XL4WPMkwC3bgoc_s1B03TSA";
 
 // API маршрут для получения данных Wildberries
 app.get("/api/stocks", async (req, res) => {
